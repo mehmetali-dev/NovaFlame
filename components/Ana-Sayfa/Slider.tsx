51575c3dc65e73003f9645f72783e42fd2f8fe95
cryptomehmett <160727214+cryptomehmett@users.noreply.github.com>
2024-11-26 13:53:15 +0300

    V10.3

--- components/Ana-Sayfa/Slider.tsx
+++ components/Ana-Sayfa/Slider.tsx
@@ -10,13 +10,13 @@ const Slider = () => {
       {/* SEO Meta Tagleri */}
       <Head>
         <title>Anasayfa - Nova Flame</title>
-        <meta name="description" content=" Gaziantep, Adana, Mersin, Kahramanmaraş, Şanlıurfa, Malatya, Adıyaman, Hatay Ve Tüm Güneydoğu Bölgesinde Odunlu şömine, barbekü, elektrikli şömine, ekmek fırınları, biogazlı şömine çeşitlerimiz ile sektörde siz müşterilerimize kaliteli, güvenilir ve ekonomik hizmetler sunmaya kararlılıkla devam etmekteyiz." />
+        <meta name="description" content="Gaziantep, Adana, Mersin, Kahramanmaraş, Şanlıurfa, Malatya, Adıyaman, Hatay Ve Tüm Güneydoğu Bölgesinde Odunlu şömine, barbekü, elektrikli şömine, ekmek fırınları, biogazlı şömine çeşitlerimiz ile sektörde siz müşterilerimize kaliteli, güvenilir ve ekonomik hizmetler sunmaya kararlılıkla devam etmekteyiz." />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="keywords" content="Gaziantep Şömine, Adana Şömine, Şanlıurfa Şömine, Hatay Şömine, Adıyaman Şömine, Güneydoğu Şömine, Elektrikli Şömine, Elektrikli Şömine Modelleri, elektrikli somine, elektrikli şömine üreticileri, elektrikli şömine modelleri ve fiyatları, elektrikli şömine 150cm, tektes şömine, elektrikli şömine malatya, elektrikli şömine çeşitleri, elektrikli somine modelleri, hazır şömine modelleri, şömine fiyatları, şömine elektrik, zaya şömine, , flameline şömine, dekoratif şömine, 150cm elektrikli şömine, hazir sömine, odun şömine fiyatları, elektrikli şömine tasarımları, elektrikli şömine dekorasyon, elektrikli şömine haznesi, dlc şömine, şömine elektrikli, portatif elektrikli şömine, elk şömine, taşınılabilir elektrikli şömine, elektrikli şömine görselleri, elektronik şömine, salon elektrikli şömine," />
+        <meta name="keywords" content="Gaziantep Şömine, Adana Şömine, Şanlıurfa Şömine, Hatay Şömine, Adıyaman Şömine, Güneydoğu Şömine, Elektrikli Şömine, Elektrikli Şömine Modelleri, elektrikli somine, elektrikli şömine üreticileri, elektrikli şömine modelleri ve fiyatları, elektrikli şömine 150cm, tektes şömine, elektrikli şömine malatya, elektrikli şömine çeşitleri, elektrikli somine modelleri, hazır şömine modelleri, şömine fiyatları, şömine elektrik, zaya şömine, flameline şömine, dekoratif şömine, 150cm elektrikli şömine, hazir sömine, odun şömine fiyatları, elektrikli şömine tasarımları, elektrikli şömine dekorasyon, elektrikli şömine haznesi, dlc şömine, şömine elektrikli, portatif elektrikli şömine, elk şömine, taşınılabilir elektrikli şömine, elektrikli şömine görselleri, elektronik şömine, salon elektrikli şömine," />
         <link rel="canonical" href="https://novasomine.com/" />
         <meta name="robots" content="index, follow" />
         <meta property="og:title" content="Anasayfa - Nova Flame" />
-        <meta property="og:description" content=" Gaziantep, Adana, Mersin, Kahramanmaraş, Şanlıurfa, Malatya, Adıyaman, Hatay Ve Tüm Güneydoğu Bölgesinde Odunlu şömine, barbekü, elektrikli şömine, ekmek fırınları, biogazlı şömine çeşitlerimiz ile sektörde siz müşterilerimize kaliteli, güvenilir ve ekonomik hizmetler sunmaya kararlılıkla devam etmekteyiz." />
+        <meta property="og:description" content="Gaziantep, Adana, Mersin, Kahramanmaraş, Şanlıurfa, Malatya, Adıyaman, Hatay Ve Tüm Güneydoğu Bölgesinde Odunlu şömine, barbekü, elektrikli şömine, ekmek fırınları, biogazlı şömine çeşitlerimiz ile sektörde siz müşterilerimize kaliteli, güvenilir ve ekonomik hizmetler sunmaya kararlılıkla devam etmekteyiz." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://novasomine.com" />
         <meta property="og:image" content="https://novasomine.com" />
@@ -28,7 +28,7 @@ const Slider = () => {
 
       <div className="my-36">
         <div className="mx-auto max-w-4xl lg:text-center max-md:text-center max-xl:text-center mb-28">
-          <h2 className="text-base font-semibold leading-7 text-[#F06B1C] ">Nova Şömine</h2>
+          <h2 className="text-base font-semibold leading-7 text-[#F06B1C]">Nova Şömine</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">Sektöre Değer Katan Ortaklar</p>
           <p className="mt-6 text-lg leading-8 text-gray-600">Nova Şömine, yenilikçi tasarımları ve kaliteli ürünleriyle sektöre değer katıyor. Ortaklarımızla birlikte, estetik ve enerji verimliliğini birleştirerek müşteri memnuniyetini ön planda tutuyoruz.</p>
         </div>
@@ -41,9 +41,7 @@ const Slider = () => {
                 key={index}
               >
                 <Link href={logo.href}>
-                  <a>
-                    <Image src={logo.svg} alt={logo.name} width={500} height={500} className="opacity-50 hover:opacity-100" />
-                  </a>
+                  <Image src={logo.svg} alt={logo.name} width={500} height={500} className="opacity-50 hover:opacity-100" />
                 </Link>
               </div>
             ))}
@@ -53,9 +51,7 @@ const Slider = () => {
                 key={index}
               >
                 <Link href={logo.href}>
-                  <a>
-                    <Image src={logo.svg} alt={logo.name} width={500} height={500} className="opacity-50 hover:opacity-100" />
-                  </a>
+                  <Image src={logo.svg} alt={logo.name} width={500} height={500} className="opacity-50 hover:opacity-100" />
                 </Link>
               </div>
             ))}
