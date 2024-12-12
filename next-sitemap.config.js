1189a9a47fe8c3a8c2d6078557a4e2e30f250038
Nova-Şömine <160727214+cryptomehmett@users.noreply.github.com>
2024-12-12 15:45:11 +0300

    V13.8

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-// next-sitemap.config.js
-module.exports = {
-    siteUrl: 'https://novasomine.com', // Buraya sitenizin URL'sini yazın
-    generateRobotsTxt: true, // Robots.txt dosyasını otomatik olarak üretir
-    changefreq: 'daily', // Sayfa değişiklik sıklığını belirler
-    priority: 0.7, // Sayfanın önceliğini belirtir
-    sitemapSize: 5000, // Sitemap dosyasındaki URL sayısı
-  };
-  
